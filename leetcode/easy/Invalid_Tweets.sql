6b4f2d18718891770d2e94d55f0c719e8a8741db
Maksim Chudakov <maksim.chudakoff@gmail.com>
2026-05-12 10:22:24 -0400

    Update Invalid_Tweets.sql

--- leetcode/easy/Invalid_Tweets.sql
+++ leetcode/easy/Invalid_Tweets.sql
@@ -4,7 +4,6 @@ Platform: LeetCode
 Problem ID: 1683
 Difficulty: Easy
 Link: https://leetcode.com/problems/invalid-tweets/
-Date Solved: 2026-05-12
 */
 
 -- Find tweet IDs where content length exceeds 15 characters
