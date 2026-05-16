57ffc86703c4eeac1255fcb587873ad7f86e134e
Maksim Chudakov <maksim.chudakoff@gmail.com>
2026-05-15 23:23:11 -0400

    Update Find_Valid_Emails.sql

--- leetcode/easy/Find_Valid_Emails.sql
+++ leetcode/easy/Find_Valid_Emails.sql
@@ -6,6 +6,10 @@ Difficulty: Easy
 Link: https://leetcode.com/problems/find-valid-emails/
 */
 
+-- Find all valid email addresses using regex pattern matching
+-- Valid format: alphanumeric/underscore before @, letters only for domain, ends with .com
+
+
 SELECT user_id, email
 FROM Users
 WHERE email ~ '^[A-Za-z0-9_]+@[A-Za-z]+\.com$'
